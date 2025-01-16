5e72c89a2bc0ce06ee2908dc68e8e573a958803a
vova <hohiop09@gmail.com>
2025-01-16 12:02:08 +0200

    installed the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <BrowserRouter></BrowserRouter>
   )
 }
 
